37363d9ac2bbb6eaa3aba7a777f5fc83881335f3
San Nge <samngestep@gmail.com>
2020-12-10 21:34:53 -0800

    adding logout on expires token

--- client/src/client/ApolloClient.js
+++ client/src/client/ApolloClient.js
@@ -7,6 +7,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 import { useAuthDispatch } from "../context/auth";
+import { onError } from "@apollo/client/link/error";
 
 export default function ApolloProvider(props) {
 	//will need to add WebsocketLink in the future for subscription
@@ -24,11 +25,18 @@ export default function ApolloProvider(props) {
 		};
 	});
 
+	const errorLink = onError(({ graphQLErrors, networkError }) => {
+		console.log(graphQLErrors && graphQLErrors[0] && graphQLErrors[0].message);
+		if (graphQLErrors?.[0]?.message === "Unauthenticated") {
+			dispatch({ type: "LOGOUT" });
+		}
+	});
+
 	const client = new ApolloClient({
 		link: authLink.concat(httpLink),
 		cache: new InMemoryCache(),
 	});
-	//not adding the logout on networkError 401 because 
+	//not adding the logout on networkError 401 because
 	//it is not stable. Trying to apply refreshing token method
 	//into the project
 	return <Provider client={client} {...props} />;
